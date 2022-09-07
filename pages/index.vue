c55207623853340f687f46d22772e1df8f37ff1e
Jainy Xavier <xavierjainy15@gmail.com>
2022-09-07 13:52:55 -0300

    feature: criação da seção de informações do site

--- pages/index.vue
+++ pages/index.vue
@@ -118,6 +118,34 @@
                 </div>
             </div>
         </section>
+
+        <section class="section-information">
+            <div class="container">
+                <div class="information">
+                    <div class="title-information">
+                        <h2>Germany-based & GDPR Compliant</h2>
+                    </div>
+                    <div class="box-information">
+                        <div class="img-information">
+                            <nuxt-img src="img/data.svg"/>
+                        </div>
+                        <p>Data does not<br> leave our servers</p>
+                    </div>
+                    <div class="box-information">
+                        <div class="img-information">
+                            <nuxt-img src="img/infrastructure.svg"/>
+                        </div>
+                        <p>Own<br> infrastructure</p>
+                    </div>
+                    <div class="box-information">
+                        <div class="img-information">
+                            <nuxt-img src="img/most.svg"/>
+                        </div>
+                        <p>Most renowed<br> data centers</p>
+                    </div>
+                </div>
+            </div>
+        </section>
     </div>
 </template>
 
